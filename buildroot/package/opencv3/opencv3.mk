bc9edbe86efc63f89b84bf2f02b8cf9db236e270
Laurent Itti <jevois.org@gmail.com>
2018-12-05 18:26:12 -0800

    opencv 4.0.0 release

--- buildroot/package/opencv3/opencv3.mk
+++ buildroot/package/opencv3/opencv3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCV3_VERSION = 4.0.0-beta
+OPENCV3_VERSION = 4.0.0
 OPENCV3_SITE = $(call github,opencv,opencv,$(OPENCV3_VERSION))
 OPENCV3_INSTALL_STAGING = YES
 OPENCV3_LICENSE = BSD-3-Clause
