d5ae55ae4dc5e1957a6b43c2f1ac0b9975124cb4
Laurent Itti <jevois.org@gmail.com>
2019-09-12 10:03:37 -0700

    opencv 4.1.1, color adjust on ar0135

--- buildroot/package/opencv_contrib/opencv_contrib.mk
+++ buildroot/package/opencv_contrib/opencv_contrib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCV_CONTRIB_VERSION = 4.1.0
+OPENCV_CONTRIB_VERSION = 4.1.1
 OPENCV_CONTRIB_SITE = $(call github,opencv,opencv_contrib,$(OPENCV_CONTRIB_VERSION))
 OPENCV_CONTRIB_INSTALL_STAGING = YES
 OPENCV_CONTRIB_LICENSE = BSD-3c
